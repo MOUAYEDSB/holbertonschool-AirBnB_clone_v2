cadbf02b70bbe75b6c7656c488f296c3b32bf7b9
MOUAYEDSB <sabbaghmouayed@gmail.com>
2023-08-03 15:05:21 +0100

    DBStorage - Place

--- models/user.py
+++ models/user.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""This module defines the User class, which inherits from BaseModel"""
+"""This module defines the User and City classes"""
 
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -14,3 +15,16 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+
+    places = relationship('Place', backref='user', cascade='all, delete')
+
+
+class City(BaseModel, Base):
+    """The City class, contains city information"""
+
+    __tablename__ = 'cities'
+
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+
+    places = relationship('Place', backref='cities', cascade='all, delete')
