42af6c65d0c128cceb272efc2cd2e12263d88fcb
MOUAYEDSB <sabbaghmouayed@gmail.com>
2023-08-03 01:22:49 +0100

    fix

--- models/user.py
+++ models/user.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+"""This module defines the User class, which inherits from BaseModel"""
+
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
+    """The User class, contains user information"""
+
     __tablename__ = 'users'
+
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-
-    def __init__(self, *args, **kwargs):
-        """Initializes User"""
-        super().__init__(*args, **kwargs)
